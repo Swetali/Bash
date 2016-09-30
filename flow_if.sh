b88bf4e65f7c1ef81125afb25e7e63c7934515a9
Swetali <swetali.patel@gmail.com>
2016-09-30 13:04:32 -0400

    added flow_if

---
+++ flow_if.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#flow_if.sh gives an example of how to use if statements in bash
+
+if [$((num1 % 2)) -eq ]; then
+	echo "first number is even"
+else 
+	echo "first number is odd"
+
+fi
+
